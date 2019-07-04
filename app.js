bb6f4a18454838afb314ab97d16ff11fdc60f3b5
syyad khan <syyadkhan@yahoo.com>
2019-07-04 11:20:32 -0400

    mongo atlas connection est

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
+
+const uri = process.env.MONGO_URI;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("Mongo Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from the server!", app: "MERN Test" });
@@ -9,6 +24,9 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   res.send(" post message test");
 });
+
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 4000;
 

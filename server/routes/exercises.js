ec38afa60f96b1a4a811e067593fecb630a5a825
Syyad Khan <syykhan@deloitte.com>
2019-12-11 19:35:36 -0500

    user routes tested

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 // Get All
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  res.send("on exercise route");
+});
 // Get One
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
 // Create One
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  console.log(res.body);
+});
 // Updating One
 router.patch("/:id", (req, res) => {});
 // Deleting One

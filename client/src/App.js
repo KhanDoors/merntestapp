534c4216a2c5408dea3356c7d127d56a1e7ef6e6
Syyad Khan <syykhan@deloitte.com>
2020-02-25 17:45:31 -0500

    materialui setup

--- client/src/App.js
+++ client/src/App.js
@@ -13,21 +13,29 @@ import Home from "./components/pages/Home";
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
 import ExerciseContextProvider from "./contexts/ExerciseContext";
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider
+} from "@material-ui/core/styles";
+import theme from "./theme";
 
 const App = () => {
   return (
     <ExerciseContextProvider>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/exercises" component={Exercises} />
-          <Route exact path="/exerciseform" component={ExerciseForm} />
-          <Route exact path="/exercise/:id" component={Exercise} />
-          <Redirect to="/" />
-        </Switch>
-        <Footer />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/exercises" component={Exercises} />
+            <Route exact path="/exerciseform" component={ExerciseForm} />
+            <Route exact path="/exercise/:id" component={Exercise} />
+            <Redirect to="/" />
+          </Switch>
+          <Footer />
+        </Router>
+      </ThemeProvider>
     </ExerciseContextProvider>
   );
 };
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { Fragment } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { Link, withRouter } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0
+  });
+}
+
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    marginBottom: "7em"
+  }
+}));
 
 function Navbar({ match }) {
+  const classes = useStyles();
+  const theme = useTheme();
   return (
-    <div>
-      <AppBar position="static" color="secondary">
-        <Toolbar>
-          <Typography variant="h6">Exercise App</Typography>
-          <div style={{ marginLeft: "auto" }}>
-            <Button variant="contained" style={{ backgroundColor: "yellow" }}>
-              <Link to="/">Home</Link>
-            </Button>{" "}
-            <Button variant="contained" style={{ backgroundColor: "orange" }}>
-              <Link to="/exercises">Exercises</Link>
-            </Button>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <Typography variant="h5">Exercise App</Typography>
+            <div style={{ marginLeft: "auto" }}>
+              <Button variant="contained" style={{ backgroundColor: "yellow" }}>
+                <Link to="/">Home</Link>
+              </Button>{" "}
+              <Button variant="contained" style={{ backgroundColor: "orange" }}>
+                <Link to="/exercises">Exercises</Link>
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </Fragment>
   );
 }
 

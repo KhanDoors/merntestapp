691ed3eb73ba456939edc5d71dcb87022ea08ca2
syyad <syyadkhan@yahoo.com>
2020-03-01 17:12:17 -0500

    sec chk

--- client/src/components/maps/Maps.js
+++ client/src/components/maps/Maps.js
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import ReactMapGL from "react-map-gl";
+import { withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+
+require("dotenv").config();
 
 const Maps = () => {
   const [viewport, setViewport] = useState({
-    width: 400,
+    width: "100vw",
     height: 400,
     latitude: 37.7577,
     longitude: -122.4376,
-    zoom: 8
+    zoom: 3
   });
 
   return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_MAPBOX_TOKEN}
-      onViewportChange={setViewport}
-    />
+    <Fragment>
+      <Grid container>
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          onViewportChange={setViewport}
+        />
+      </Grid>
+    </Fragment>
   );
 };
 

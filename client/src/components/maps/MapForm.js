b84a9831ece18c4b1e67d416575f794338f33732
Syyad Khan <syykhan@deloitte.com>
2020-03-02 12:20:29 -0500

    mapform done

---
+++ client/src/components/maps/MapForm.js
@@ -0,0 +1,65 @@
+import React, { Fragment } from "react";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: 400
+    }
+  }
+}));
+
+const MapForm = () => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <Card>
+        <CardActionArea style={{ margin: ".5em" }}>
+          <Grid container justify="center">
+            <Typography
+              style={{ fontWeight: "bold", color: "#355B8C" }}
+              variant="h4"
+            >
+              Add Place
+            </Typography>
+          </Grid>
+
+          <form className={classes.root}>
+            <TextField
+              required
+              itemType="text"
+              placeholder="Title"
+              name="name"
+              //   value={title}
+            />
+            <TextField
+              itemType="text"
+              multiline
+              placeholder="Description"
+              name="description"
+              //   value={description}
+            />
+            <TextField
+              itemType="url"
+              placeholder="Image"
+              name="image"
+              //   value={image}
+            />
+            <Button variant="contained" color="secondary" type="submit">
+              Submit
+            </Button>
+          </form>
+        </CardActionArea>
+      </Card>
+    </Fragment>
+  );
+};
+
+export default MapForm;
--- client/src/components/maps/Maps.js
+++ client/src/components/maps/Maps.js
@@ -3,23 +3,35 @@ import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { MapsContext } from "../../contexts/MapsContext";
+import MapForm from "./MapForm";
 
 require("dotenv").config();
 
 const Maps = () => {
   const { pinEntries, getPinEntries } = useContext(MapsContext);
   const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: 800,
     latitude: 37.6,
     longitude: -95.665,
-    zoom: 1
+    zoom: 3
   });
 
   useEffect(() => {
     getPinEntries();
+
+    // eslint-disable-next-line
   }, []);
+
+  const showAddMarkerPopup = e => {
+    const [longitude, latitude] = e.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude
+    });
+  };
 
   return (
     <Fragment>
@@ -44,6 +56,7 @@ const Maps = () => {
           mapStyle="mapbox://styles/syyad/ck79llrp80x6w1ipjodz6mt64"
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
           onViewportChange={setViewport}
+          onDblClick={showAddMarkerPopup}
         >
           {pinEntries.map(pin => (
             <Fragment>
@@ -54,35 +67,87 @@ const Maps = () => {
                 offsetLeft={-12}
                 offsetTop={-24}
               >
-                <svg
-                  // transform="translate((-50%), (-100%))"
-                  viewBox="0 0 24 24"
-                  width="24px"
-                  height="24px"
-                  stroke="red"
-                  strokeWidth="2.5"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                <div
+                  onClick={() =>
+                    setShowPopup({
+                      [pin._id]: true
+                    })
+                  }
                 >
-                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                  <circle cx="12" cy="10" r="3"></circle>
-                </svg>
+                  <svg
+                    // transform="translate((-50%), (-100%))"
+                    viewBox="0 0 24 24"
+                    width="24px"
+                    height="24px"
+                    stroke="#30F2F2"
+                    strokeWidth="2.5"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                    <circle cx="12" cy="10" r="3"></circle>
+                  </svg>
+                </div>
               </Marker>
               {showPopup[pin._id] ? (
                 <Popup
-                  latitude={37.78}
-                  longitude={-122.41}
+                  latitude={pin.latitude}
+                  longitude={pin.longitude}
                   closeButton={true}
+                  dynamicPosition={true}
                   closeOnClick={false}
-                  onClose={() => this.setState({ showPopup: false })}
+                  onClose={() => setShowPopup({})}
                   anchor="top"
                 >
-                  <div>You are here</div>
+                  <div style={{ maxWidth: "300px" }}>
+                    <h3>{pin.title}</h3>
+                    <p>{pin.description}</p>
+                  </div>
                 </Popup>
               ) : null}
             </Fragment>
           ))}
+          {addEntryLocation ? (
+            <Fragment>
+              <Marker
+                latitude={addEntryLocation.latitude}
+                longitude={addEntryLocation.longitude}
+                offsetLeft={-12}
+                offsetTop={-24}
+              >
+                <div>
+                  <svg
+                    // transform="translate((-50%), (-100%))"
+                    viewBox="0 0 24 24"
+                    width="24px"
+                    height="24px"
+                    stroke="red"
+                    strokeWidth="2.5"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                    <circle cx="12" cy="10" r="3"></circle>
+                  </svg>
+                </div>
+              </Marker>
+              <Popup
+                latitude={addEntryLocation.latitude}
+                longitude={addEntryLocation.longitude}
+                closeButton={true}
+                dynamicPosition={true}
+                closeOnClick={false}
+                onClose={() => setAddEntryLocation(null)}
+                anchor="top"
+              >
+                <div style={{ maxWidth: "400px" }}>
+                  <MapForm />
+                </div>
+              </Popup>
+            </Fragment>
+          ) : null}
         </ReactMapGL>
       </Grid>
     </Fragment>

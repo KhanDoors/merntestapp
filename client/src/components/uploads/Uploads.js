49ad4d7bd059547f20c463a4c9e0523b2693889e
syyad <syyadkhan@yahoo.com>
2020-03-20 20:56:49 -0400

    start style uploads

--- client/src/components/uploads/Uploads.js
+++ client/src/components/uploads/Uploads.js
@@ -2,8 +2,30 @@ import React, { useState } from "react";
 import Dropzone from "react-dropzone";
 import axios from "axios";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import IconButton from "@material-ui/core/IconButton";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    justify: "center",
+    alignItems: "center"
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: theme.spacing(2)
+  }
+}));
 
 const Uploads = () => {
+  const classes = useStyles();
+
   const [images, setImages] = useState([]);
   const [files, setFiles] = useState([]);
 
@@ -28,7 +50,8 @@ const Uploads = () => {
     try {
       const res = await axios
         .post("http://localhost:4000/upload", formData, config)
-        .then(res => setImages(res.data));
+        .then(res => setImages(res.data))
+        .then(setFiles([]));
     } catch (err) {
       console.log(err);
     }
@@ -85,41 +108,56 @@ const Uploads = () => {
   ));
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
-        {({ getRootProps, getInputProps }) => (
-          <div
-            style={{
-              width: "500px",
-              height: "240px",
-              border: "1px solid lightgray",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center"
-            }}
-            {...getRootProps()}
-          >
-            <input {...getInputProps()} />
-            <div style={{ display: "column" }}>
-              <div style={{ textAlign: "center" }}>
-                <CloudUploadIcon color="secondary" fontSize="large" />
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: "500px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <div style={{ display: "column" }}>
+                  <div style={{ textAlign: "center" }}>
+                    <CloudUploadIcon color="secondary" fontSize="large" />
+                  </div>
+                  <div>
+                    <h3>
+                      Drag 'n' drop some files here, or click to select files
+                    </h3>
+                  </div>
+                </div>
               </div>
-              <div>
-                <h3>Drag 'n' drop some files here, or click to select files</h3>
-              </div>
-            </div>
-          </div>
-        )}
-      </Dropzone>
-      <aside>
-        <h4>Selected Files:</h4>
-        <ul>{filepath}</ul>
-      </aside>
-      <aside style={thumbsContainer}>{thumbs}</aside>
+            )}
+          </Dropzone>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <aside>
+            <h4>Selected Files:</h4>
+            <ul>{filepath}</ul>
+          </aside>
+          <aside style={thumbsContainer}>{thumbs}</aside>
+        </Grid>
 
-      <div>
-        <button onClick={onSubmit}>Upload</button>
-      </div>
+        <Button color="primary" variant="contained" onClick={onSubmit}>
+          Upload
+        </Button>
+        <IconButton
+          color="primary"
+          aria-label="upload picture"
+          component="span"
+        >
+          <PhotoCamera />
+        </IconButton>
+      </Grid>
     </div>
   );
 };

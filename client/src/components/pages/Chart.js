3e901cb9cf33d5a7dc4c02e55470b5c5cad19a1e
Syyad Khan <syykhan@deloitte.com>
2020-03-23 16:44:43 -0400

    chart work

--- client/src/components/pages/Chart.js
+++ client/src/components/pages/Chart.js
@@ -1,16 +1,86 @@
 import React from "react";
-import Charts from "../chart/Charts";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import BarChart from "../chart/BarChart";
 import Navbar from "../layouts/Navbar";
 import Container from "@material-ui/core/Container";
+import PieChart from "../chart/PieChart";
+import DoughnutChart from "../chart/DoughnutChart";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
-const Chart = () => {
+export default function Chart() {
   return (
     <Navbar>
-      <Container style={{ height: "78vh" }}>
-        <Charts />
+      <Container>
+        <div>
+          <Grid
+            container
+            style={{ marginBottom: "1em", marginTop: "1em" }}
+            justify="center"
+          >
+            <Grid
+              style={{ marginBottom: "1em", marginTop: "1em" }}
+              container
+              justify="center"
+            >
+              <Typography
+                style={{ fontWeight: "bold", color: "#355B8C" }}
+                variant="h4"
+              >
+                Chart Library
+              </Typography>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <BarChart />
+              </Card>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <PieChart />
+              </Card>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <DoughnutChart />
+              </Card>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <BarChart />
+              </Card>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <PieChart />
+              </Card>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <DoughnutChart />
+              </Card>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <PieChart />
+              </Card>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <BarChart />
+              </Card>
+            </Grid>
+            <Grid item xs={3} style={{ margin: "1em" }}>
+              <Card>
+                <DoughnutChart />
+              </Card>
+            </Grid>
+          </Grid>
+        </div>
       </Container>
     </Navbar>
   );
-};
-
-export default Chart;
+}

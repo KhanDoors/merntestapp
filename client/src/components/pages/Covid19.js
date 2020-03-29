fc22531ec9823e15ebf8638a45cfabdeabb9b6af
syyad <syyadkhan@yahoo.com>
2020-03-29 17:35:09 -0400

    covid mapped

--- client/src/components/pages/Covid19.js
+++ client/src/components/pages/Covid19.js
@@ -3,14 +3,59 @@ import Navbar from "../layouts/Navbar";
 import Container from "@material-ui/core/Container";
 import Covid from "../covid19/Covid";
 import Countries from "../covid19/Countries";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  }
+}));
 
 const Covid19 = () => {
+  const classes = useStyles();
+
   return (
     <Navbar>
-      <Container style={{ width: "100vw" }}>
-        <Covid />
-        <Countries />
+      <Container style={{ marginBottom: "1em" }}>
+        <Grid
+          style={{ marginBottom: "1em", marginTop: "1em" }}
+          container
+          justify="center"
+        >
+          <Typography
+            style={{ fontWeight: "bold", color: "#355B8C" }}
+            variant="h2"
+          >
+            Covid - 19 Update
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid>
+            <Covid />
+          </Grid>
+        </Grid>
       </Container>
+      <br />
+      <Grid
+        style={{ marginBottom: "1em", marginTop: "1em" }}
+        container
+        justify="center"
+      >
+        <Typography
+          style={{ fontWeight: "bold", color: "#355B8C" }}
+          variant="h4"
+        >
+          Country Update
+        </Typography>
+      </Grid>
+      <br />
+      <Grid container justify="center">
+        <Grid item style={{ marginBottom: "1em", marginTop: "1em" }}>
+          <Countries />
+        </Grid>
+      </Grid>
     </Navbar>
   );
 };

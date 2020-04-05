b4be740ab183fd8f058aee87759f79d24aed15a7
syyad <syyadkhan@yahoo.com>
2020-04-05 17:32:07 -0400

    loading

--- client/src/components/covid19/Covid.js
+++ client/src/components/covid19/Covid.js
@@ -12,159 +12,170 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   card: {
     height: "27em",
-    width: "25em"
+    width: "25em",
   },
   media: {
     height: 0,
-    paddingTop: "56.25%" // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   expand: {
     transform: "rotate(0deg)",
     marginLeft: "auto",
     transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
+      duration: theme.transitions.duration.shortest,
+    }),
   },
   expandOpen: {
-    transform: "rotate(180deg)"
+    transform: "rotate(180deg)",
   },
   avatar: {
-    backgroundColor: red[500]
-  }
+    backgroundColor: red[500],
+  },
 }));
 
 export default function Covid() {
   const classes = useStyles();
   const [spacing, setSpacing] = useState(2);
   const [latest, setLatest] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getExercises = async () => {
+    try {
+      const res = await axios
+        .get("https://corona.lmao.ninja/all")
+        .then((res) => setLatest(res.data))
+        .then(setLoading(false));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    axios
-      .get("https://corona.lmao.ninja/all")
-      .then(res => {
-        setLatest(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    getExercises();
   }, []);
 
-  let timestamp = new Date(parseInt(latest.updated));
-  let today = timestamp.toString();
+  let today = new Date(latest.updated).toString();
 
   return (
     <Grid container className={classes.root} spacing={2}>
-      <Grid item xs={12}>
-        <Grid container justify="center" spacing={spacing}>
-          <Grid item>
-            <Card className={classes.card}>
-              <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    <span role="img" aria-label="covid19">
+      {latest !== null && loading !== true ? (
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={spacing}>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardHeader
+                  avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                      <span role="img" aria-label="covid19">
+                        {" "}
+                        📈
+                      </span>
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton aria-label="settings">
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title="Cases as of:"
+                  subheader={today}
+                />
+                <CardMedia
+                  className={classes.media}
+                  image="https://ewscripps.brightspotcdn.com/dims4/default/d396a36/2147483647/strip/true/crop/1280x720+0+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F3f%2Ffc%2F9f1d34714b5a9ddf0ea13a697a79%2Fcoronavirus-cases2.jpg"
+                  title="Paella dish"
+                />
+                <CardContent>
+                  <Typography variant="body2" component="p">
+                    The coronavirus COVID-19 is affecting 199 countries and
+                    territories around the world. <br />
+                    The Total Number of Cases is:
+                    <strong style={{ color: "red" }}> {latest.cases}</strong>
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardHeader
+                  avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                      <span role="img" aria-label="covid19">
+                        {" "}
+                        📈
+                      </span>
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton aria-label="settings">
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title="Deaths as of:"
+                  subheader={today}
+                />
+                <CardMedia
+                  className={classes.media}
+                  image="https://media.graytvinc.com/images/317_coviddeath_MGN.jpg"
+                  title="Codid19Deaths"
+                />
+                <CardContent>
+                  <Typography variant="body2" component="p">
+                    The COVID-19 outbreak is an unprecedented global public
+                    health challenge.
+                    <br /> The Total Number of Deaths is:
+                    <strong style={{ color: "red" }}> {latest.deaths}</strong>
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardHeader
+                  avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                      <span role="img" aria-label="covid19">
+                        {" "}
+                        📈
+                      </span>
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton aria-label="settings">
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title="Recoveries as of:"
+                  subheader={today}
+                />
+                <CardMedia
+                  className={classes.media}
+                  image="https://images.foxtv.com/static.fox26houston.com/www.fox26houston.com/content/uploads/2020/03/764/432/RECOVERED.jpg?ve=1&tl=1"
+                  title="Paella dish"
+                />
+                <CardContent>
+                  <Typography variant="body2" component="p">
+                    Recoveries are rising steadily. <br />
+                    The Total Number of Recovered is:
+                    <strong style={{ color: "red" }}>
                       {" "}
-                      📈
-                    </span>
-                  </Avatar>
-                }
-                action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
-                }
-                title="Cases as of:"
-                subheader={today}
-              />
-              <CardMedia
-                className={classes.media}
-                image="https://ewscripps.brightspotcdn.com/dims4/default/d396a36/2147483647/strip/true/crop/1280x720+0+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F3f%2Ffc%2F9f1d34714b5a9ddf0ea13a697a79%2Fcoronavirus-cases2.jpg"
-                title="Paella dish"
-              />
-              <CardContent>
-                <Typography variant="body2" component="p">
-                  The coronavirus COVID-19 is affecting 199 countries and
-                  territories around the world. <br />
-                  The Total Number of Cases is:
-                  <strong style={{ color: "red" }}> {latest.cases}</strong>
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card className={classes.card}>
-              <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    <span role="img" aria-label="covid19">
-                      {" "}
-                      📈
-                    </span>
-                  </Avatar>
-                }
-                action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
-                }
-                title="Deaths as of:"
-                subheader={today}
-              />
-              <CardMedia
-                className={classes.media}
-                image="https://media.graytvinc.com/images/317_coviddeath_MGN.jpg"
-                title="Codid19Deaths"
-              />
-              <CardContent>
-                <Typography variant="body2" component="p">
-                  The COVID-19 outbreak is an unprecedented global public health
-                  challenge.
-                  <br /> The Total Number of Deaths is:
-                  <strong style={{ color: "red" }}> {latest.deaths}</strong>
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card className={classes.card}>
-              <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    <span role="img" aria-label="covid19">
-                      {" "}
-                      📈
-                    </span>
-                  </Avatar>
-                }
-                action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
-                }
-                title="Recoveries as of:"
-                subheader={today}
-              />
-              <CardMedia
-                className={classes.media}
-                image="https://images.foxtv.com/static.fox26houston.com/www.fox26houston.com/content/uploads/2020/03/764/432/RECOVERED.jpg?ve=1&tl=1"
-                title="Paella dish"
-              />
-              <CardContent>
-                <Typography variant="body2" component="p">
-                  Recoveries are rising steadily. <br />
-                  The Total Number of Recovered is:
-                  <strong style={{ color: "red" }}> {latest.recovered}</strong>
-                </Typography>
-              </CardContent>
-            </Card>
+                      {latest.recovered}
+                    </strong>
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      ) : (
+        <div>Loading...</div>
+      )}
     </Grid>
   );
 }

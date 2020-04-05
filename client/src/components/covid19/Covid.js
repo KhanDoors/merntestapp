e6f4fe737640c436a0bb12de13e04eba12cbac92
syyad <syyadkhan@yahoo.com>
2020-04-05 17:50:57 -0400

    cleanup

--- client/src/components/covid19/Covid.js
+++ client/src/components/covid19/Covid.js
@@ -24,16 +24,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
   avatar: {
     backgroundColor: red[500],
   },

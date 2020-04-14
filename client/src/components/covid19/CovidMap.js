7129a9b19d1c8e76e77fcc0450ee7e8ac66171ab
Syyad Khan <syykhan@deloitte.com>
2020-04-13 20:43:08 -0400

    import cleanup

--- client/src/components/covid19/CovidMap.js
+++ client/src/components/covid19/CovidMap.js
@@ -1,19 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import TextField from "@material-ui/core/TextField";
-import Container from "@material-ui/core/Container";
 import GoogleMapReact from "google-map-react";
 import CountUp from "react-countup";
 
